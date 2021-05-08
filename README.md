21ea1fcbe94d71d7697f74b19a496386936d6de9
fbalestrini <gaojing@hotmail.it>
2021-05-08 12:48:21 +0200

    First version

---
+++ README.md
@@ -0,0 +1,28 @@
+# bThings GPIO Library
+## Overview
+This Mongoose-OS library allows you to easily a [bThing](https://github.com/diy365-mgos/bthing) to GPIO.
+## GET STARTED
+Build up your own device in few minutes just starting from the following sample. Start including following libraries into your `mos.yml` file.
+```yaml
+libs:
+  - origin: https://github.com/diy365-mgos/bthing-gpio
+```
+**C/C++ sample code**
+```c
+#include "mgos.h"
+#include "mgos_bthing_gpio.h"
+```
+## C/C++ API Reference
+### mgos_bthing_gpio_attach
+```c
+bool mgos_bthing_gpio_attach(mgos_bthing_t thing, int pin, enum mgos_gpio_pull_type pull);
+};
+```
+Attaches a *bThing* to the specified GPIO. Returns `true` on success, or `false` otherwise.
+
+|Property||
+|--|--|
+|thing|A *bThing*.|
+|pin|The GPIO pin.|
+|pull|The GPIO pull type: `MGOS_GPIO_PULL_UP` or `MGOS_GPIO_PULL_DOWN`.|
+
---
+++ src/mgos_bthing_gpio.c
@@ -0,0 +1,75 @@
+#include "mgos.h"
+#include "mgos_bthing.h"
+
+#ifdef MGOS_HAVE_MJS
+#include "mjs.h"
+#endif
+
+struct mg_bthing_gpio_cfg {
+  int pin;
+  enum mgos_gpio_pull_type pull;
+};
+
+#if MGOS_BTHING_HAVE_SENSORS
+bool mg_bthing_gpio_get_state_cb(mgos_bthing_t thing, mgos_bvar_t state, void *userdata) {
+  struct mg_bthing_gpio_cfg *cfg = (struct mg_bthing_gpio_cfg *)userdata;
+  if (thing && state && cfg) {
+    bool gpio_is_on = mgos_gpio_read(cfg->pin);
+    mgos_bvar_set_bool(state, (cfg->pull == MGOS_GPIO_PULL_UP ? !gpio_is_on : gpio_is_on));
+    return true;
+  }
+  return false;
+}
+#endif // MGOS_BTHING_HAVE_SENSORS
+
+#if MGOS_BTHING_HAVE_ACTUATORS
+bool mg_bthing_gpio_set_state_cb(mgos_bthing_t thing, mgos_bvarc_t state, void *userdata) {
+  struct mg_bthing_gpio_cfg *cfg = (struct mg_bthing_gpio_cfg *)userdata;
+  if (thing && cfg && (mgos_bvar_get_type(state) == MGOS_BVAR_TYPE_BOOL)) {
+    bool gpio_set_on = mgos_bvar_get_bool(state);
+    mgos_gpio_write(cfg->pin, (cfg->pull == MGOS_GPIO_PULL_UP ? !gpio_set_on : gpio_set_on));
+    return true;
+  }
+  return false;
+}
+#endif // MGOS_BTHING_HAVE_ACTUATORS
+
+bool mgos_bthing_gpio_attach(mgos_bthing_t thing, int pin, enum mgos_gpio_pull_type pull) {
+  #if MGOS_BTHING_HAVE_SENSORS
+
+  if (mgos_bthing_is_typeof(thing, MGOS_BTHING_TYPE_ACTUATOR)) {
+    if (!mgos_gpio_set_pull(pin, pull)) return false;
+    if (!mgos_gpio_setup_output(pin, (pull == MGOS_GPIO_PULL_UP ? true : false))) return false;
+  } else if (mgos_bthing_is_typeof(thing, MGOS_BTHING_TYPE_SENSOR)) {
+    if (!mgos_gpio_setup_input(pin, pull)) return false;
+  }
+  
+  char set_count = 0;
+  struct mg_bthing_gpio_cfg *cfg = calloc(1, sizeof(struct mg_bthing_gpio_cfg));
+  cfg->pin = pin;
+  cfg->pull = pull;
+ 
+  if (mgos_bthing_is_typeof(thing, MGOS_BTHING_TYPE_SENSOR)) {
+    if(mgos_bthing_on_get_state(thing, mg_bthing_gpio_get_state_cb, cfg)) ++set_count;
+  }
+
+  #if MGOS_BTHING_HAVE_ACTUATORS
+  if (mgos_bthing_is_typeof(thing, MGOS_BTHING_TYPE_ACTUATOR) && (set_count > 0)) {
+    if (!mgos_bthing_on_set_state(thing, mg_bthing_gpio_set_state_cb, cfg)) {
+      mgos_bthing_on_get_state(thing, NULL, NULL);
+    }
+  }
+  
+  if (!set_count) free(cfg);
+  return (set_count > 0);
+
+  #endif // MGOS_BTHING_HAVE_ACTUATORS
+  #endif //MGOS_BTHING_HAVE_SENSORS
+
+  return false;
+}
+
+
+bool mgos_bthing_gpio_init() {
+  return true;
+}
